333d0a257e63c40484af57ff955e667fcbb23943
Kyle Nakamura <kjnakamura@apu.edu>
2019-02-03 03:45:51 -0800

    Set up style for TodoItem.js with getStyle()

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		todos: [
+			{
+				id: 1,
+				title: 'Take out the trash',
+				completed: false
+			},
+			{
+				id: 2,
+				title: 'Dinner with the fam',
+				completed: true
+			},
+			{
+				id: 3,
+				title: 'Meeting with boss',
+				completed: false
+			}
+		]
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Todos todos={this.state.todos} />
+			</div>
+		);
+	}
 }
 
 export default App;
